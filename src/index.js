95b8167271d2167b1618fecd85cbe9f0849150f4
simonDungeon <abhinabsaikia13@yahoo.com>
2020-07-29 18:56:00 +0530

    Edited the index.js and index.css files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css' 
 
-const Person = () =>{
+const Person = ({img, name, job, children}) =>{
+  const url = `https://randomuser.me/api/portraits/med/men/${img}.jpg`
   return (
-    <article>
-      <img src = "https://picsum.photos/200/300" alt = "Random image1"/>
-    <h4>Name</h4>
-    <h4>Job</h4>
+    <article className = "person">
+      <img src = {url} 
+        alt = "Random"
+      />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
     </article>
   );
 }
 
 const PersonList = () => {
   return (
-  <section>
-    <Person/>
+  <section className = "person-list">
+    <Person img = "34" name="Abhinab" job="Developer"/>
+    <Person img = "22" name="Simran" job="Developer">
+      <p>
+        Children Paragraph
+      </p>
+    </Person>
+    <Person img = "13" name="Ankita" job="Designer"/>
   </section>);
 };
 
